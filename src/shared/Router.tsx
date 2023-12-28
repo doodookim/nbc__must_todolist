ae7a3f7c2eba70ac0595db6aebe23d020e1bdf3c
eiansd <joh7727@naver,com>
2023-12-28 11:49:43 +0900

    공유하기버튼

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "../pages/Login";
-import Main from "../pages/Main";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Share from '../components/Share';
+import Login from '../pages/Login';
+import Main from '../pages/Main';
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element = {<Main/>} />
-                <Route path="Login" element = {<Login/>} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+
+        <Route path="Login" element={<Login />} />
+        <Route path="Share" element={<Share />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
