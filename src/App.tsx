090abe48f2f86a42c2a302937945e249e2acde58
eiansd <joh7727@naver,com>
2023-12-31 17:49:53 +0900

    머지

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import Router from './shared/Router';
 import GlobalStyle from './styled/GlobalStyle';
@@ -7,21 +6,8 @@ function App() {
   return (
     <>
       <GlobalStyle />
-
       <Router />
     </>
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
->>>>>>> 38563b20a9a2408489b195db6dcb9570e929c3a7
   );
 }
 
