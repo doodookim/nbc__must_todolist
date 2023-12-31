27ab2c722e0857f77c019fbe01c0bd7117080226
eiansd <joh7727@naver,com>
2023-12-31 17:27:48 +0900

    저장

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -11,9 +11,9 @@ const Router = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Main />} />
-          <Route path="Login" element={<Login />} />
-          <Route path="Share" element={<Share />} />
-          <Route path="Alram" element={<Alram />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Share" element={<Share />} />
+          <Route path="/Alram" element={<Alram />} />
         </Route>
       </Routes>
     </BrowserRouter>
